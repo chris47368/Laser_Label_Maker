952dd7348d7491d3e59db9bfc5d7935358001501
chris47368 <37160228+chris47368@users.noreply.github.com>
2022-07-27 03:49:42 +0100

    Add files via upload
    First version - much work remaining to do on it

---
+++ Laser Label Maker v0.1/Laser Label Maker/Form1.vb
@@ -0,0 +1,77 @@
+﻿Public Class Form1
+    Dim presets As String() = {"Red 650nm", "Green 532nm", "Blue 445nm", "Blue 450nm", "Violet 405nm"}
+    Dim preset_wavelenth As String() = {"650nm", "532nm", "445nm", "450nm", "405nm"}
+    Dim output_power As Double = 5
+    Dim power_units As String() = {"nW", "μW", "mW", "W", "KW", "MW", "GW", "TW"}
+    Dim laser_class As String() = {"Class IIIb Laser Product", "Class IV Laser Product"}
+    Dim power_unit_index As Integer = 2
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Dim wavelengths As Byte(,) = {{0, 0, 255, 255}, {0, 255, 0, 255}, {255, 0, 0, 255}, {240, 0, 0, 255}, {255, 0, 100, 255}}
+        '                             0,0,255,255  0,255,0,255  255,0,0,255 240,0,0,255 255,0,100,255
+        Dim match As Boolean = False
+        For i As Integer = 0 To 4
+            If ComboBox1.Text = presets(i) Then
+                Dim b_value(3) As Byte
+                For x As Integer = 0 To 3
+                    b_value(x) = wavelengths(i, x)
+                Next
+                Panel1.BackColor = Color.FromArgb(BitConverter.ToInt32(b_value, 0))
+                Wavelength_text.Text = preset_wavelenth(i)
+            End If
+        Next
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        If TextBox1.TextLength > 0 Then
+            Try
+                output_power = TextBox1.Text
+                OutputPower_text.Text = output_power.ToString & ComboBox2.Text
+                If CheckBox2.Checked = True Then
+                    OutputPower_text.Text = "<" & OutputPower_text.Text
+                End If
+            Catch ex As Exception
+
+                TextBox1.Text = output_power
+            End Try
+        End If
+        If (output_power >= 500 And power_unit_index = 2) Or power_unit_index > 2 Then
+            LaserClass_Text.Text = laser_class(1)
+        Else
+            LaserClass_Text.Text = laser_class(0)
+        End If
+
+    End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+        OutputPower_text.Text = output_power.ToString & ComboBox2.Text
+        If CheckBox2.Checked = True Then
+            OutputPower_text.Text = "<" & OutputPower_text.Text
+        End If
+        For i As Integer = 0 To power_units.Length - 1
+            If ComboBox2.Text = power_units(i) Then
+                power_unit_index = i
+            End If
+        Next
+        If (output_power >= 500 And power_unit_index = 2) Or power_unit_index > 2 Then
+            LaserClass_Text.Text = laser_class(1)
+        Else
+            LaserClass_Text.Text = laser_class(0)
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox2.Checked = True Then
+            OutputPower_text.Text = "<" & OutputPower_text.Text
+        Else
+            OutputPower_text.Text = Mid(OutputPower_text.Text, 2)
+        End If
+    End Sub
+End Class
