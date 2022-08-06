e484e0593bdd30048f7440f11506626e19da4271
chris47368 <37160228+chris47368@users.noreply.github.com>
2022-08-06 03:19:44 +0100

    Add files via upload
    ###Improvements###
    
    - Cleaned up boarder edge around laser label preview
    - Added Class II laser classification
    
    ###Functionality###
    
    - 'Save Label' button is now functional! Current file output type is that of a .png image

---
+++ Laser Label Maker v0.2/Laser Label Maker/Form1.Designer.vb
@@ -0,0 +1,282 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.LaserClass_Text = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Wavelength_text = New System.Windows.Forms.Label()
+        Me.OutputPower_text = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Custom_WL_Text = New System.Windows.Forms.TextBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.Panel1.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Red
+        Me.Panel1.Controls.Add(Me.Panel3)
+        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Location = New System.Drawing.Point(42, 26)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(621, 383)
+        Me.Panel1.TabIndex = 0
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.White
+        Me.Panel3.Controls.Add(Me.LaserClass_Text)
+        Me.Panel3.Location = New System.Drawing.Point(0, 328)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(626, 62)
+        Me.Panel3.TabIndex = 1
+        '
+        'LaserClass_Text
+        '
+        Me.LaserClass_Text.AutoSize = True
+        Me.LaserClass_Text.BackColor = System.Drawing.Color.Transparent
+        Me.LaserClass_Text.Font = New System.Drawing.Font("Calibri", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LaserClass_Text.Location = New System.Drawing.Point(216, 3)
+        Me.LaserClass_Text.Name = "LaserClass_Text"
+        Me.LaserClass_Text.Size = New System.Drawing.Size(270, 33)
+        Me.LaserClass_Text.TabIndex = 2
+        Me.LaserClass_Text.Text = "Class IIIb Laser Product"
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.Transparent
+        Me.Panel2.BackgroundImage = Global.Laser_Label_Maker.My.Resources.Resources.Warning
+        Me.Panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Panel2.Controls.Add(Me.Wavelength_text)
+        Me.Panel2.Controls.Add(Me.OutputPower_text)
+        Me.Panel2.Location = New System.Drawing.Point(0, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(626, 328)
+        Me.Panel2.TabIndex = 0
+        '
+        'Wavelength_text
+        '
+        Me.Wavelength_text.AutoSize = True
+        Me.Wavelength_text.BackColor = System.Drawing.Color.Transparent
+        Me.Wavelength_text.Font = New System.Drawing.Font("Calibri", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Wavelength_text.Location = New System.Drawing.Point(393, 296)
+        Me.Wavelength_text.Name = "Wavelength_text"
+        Me.Wavelength_text.Size = New System.Drawing.Size(82, 29)
+        Me.Wavelength_text.TabIndex = 1
+        Me.Wavelength_text.Text = "650nm"
+        '
+        'OutputPower_text
+        '
+        Me.OutputPower_text.AutoSize = True
+        Me.OutputPower_text.Font = New System.Drawing.Font("Calibri", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.OutputPower_text.Location = New System.Drawing.Point(481, 267)
+        Me.OutputPower_text.Name = "OutputPower_text"
+        Me.OutputPower_text.Size = New System.Drawing.Size(67, 29)
+        Me.OutputPower_text.TabIndex = 0
+        Me.OutputPower_text.Text = "5mW"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Red 650nm", "Green 532nm", "Blue 445nm", "Blue 450nm", "Violet 405nm"})
+        Me.ComboBox1.Location = New System.Drawing.Point(169, 19)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(156, 21)
+        Me.ComboBox1.TabIndex = 1
+        Me.ComboBox1.Text = "Red 650nm"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Custom_WL_Text)
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 457)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(358, 77)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Laser Colour/Wavelenth"
+        '
+        'Custom_WL_Text
+        '
+        Me.Custom_WL_Text.Enabled = False
+        Me.Custom_WL_Text.Location = New System.Drawing.Point(182, 44)
+        Me.Custom_WL_Text.MaxLength = 5
+        Me.Custom_WL_Text.Name = "Custom_WL_Text"
+        Me.Custom_WL_Text.Size = New System.Drawing.Size(100, 20)
+        Me.Custom_WL_Text.TabIndex = 5
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Enabled = False
+        Me.CheckBox1.Location = New System.Drawing.Point(18, 47)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(158, 17)
+        Me.CheckBox1.TabIndex = 5
+        Me.CheckBox1.Text = "Use Custom Wavelenth(nm)"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(15, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(148, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Choose a preset Wavelength:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.CheckBox2)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.TextBox1)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.ComboBox2)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 540)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(358, 70)
+        Me.GroupBox2.TabIndex = 5
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Laser Output power"
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(182, 18)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(32, 17)
+        Me.CheckBox2.TabIndex = 7
+        Me.CheckBox2.Text = "<"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(11, 42)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Unit:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(83, 15)
+        Me.TextBox1.MaxLength = 5
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(93, 20)
+        Me.TextBox1.TabIndex = 8
+        Me.TextBox1.Text = "5"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(11, 19)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(75, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Output Power:"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"nW", "μW", "mW", "W", "KW", "MW", "GW", "TW"})
+        Me.ComboBox2.Location = New System.Drawing.Point(46, 39)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(69, 21)
+        Me.ComboBox2.TabIndex = 6
+        Me.ComboBox2.Text = "mW"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 624)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(687, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Save Label"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'SaveFileDialog1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(711, 659)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Laser label maker v0.2"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents LaserClass_Text As Label
+    Friend WithEvents Wavelength_text As Label
+    Friend WithEvents OutputPower_text As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Custom_WL_Text As TextBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+End Class
---
+++ Laser Label Maker v0.2/Laser Label Maker/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Laser Label Maker")>
+<Assembly: AssemblyDescription("A useful tool to create laser warning labels/stickers")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Laser Label Maker")>
+<Assembly: AssemblyCopyright("Copyright chris47368@github.com © 2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("71b910d9-6f24-401a-9980-e424c9677861")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("0.0.0.2")>
+<Assembly: AssemblyFileVersion("0.0.0.2")>
